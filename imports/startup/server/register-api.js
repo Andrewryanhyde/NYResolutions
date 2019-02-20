bfc7dee48490becaba874ab0c01eb2cee4506f09
Andrewryanhyde <andrewryanhyde@hotmail.co.uk>
2019-02-20 15:12:35 +0000

    First object query

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -7,19 +7,29 @@ import ResolutionsSchema from "../../api/resolutions/resolutions.graphql";
 const testSchema = `
 type Query {
        hi: String
+       resolutions: [Resolution]
 }
 `;
 
-const typeDefs = [
-       testSchema,
-       ResolutionsSchema
-];
+const typeDefs = [testSchema, ResolutionsSchema];
  
 
 const resolvers  = {
        Query: {
               hi() {
                      return "Hello Level Up";
+              },
+              resolutions() {
+                     return [
+                            {
+                                   _id: "asdfasdfasdf",
+                                   name: "Get stuff done!"
+                            },
+                            {
+                                   _id: "afloogelhorn",
+                                   name: "Get stuff done!"
+                            }
+                     ];
               }
        }
 };
