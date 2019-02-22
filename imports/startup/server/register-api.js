e34cffa129c268101481488f95435cdc14f94c6d
Andrewryanhyde <andrewryanhyde@hotmail.co.uk>
2019-02-22 16:22:46 +0000

    Implemented data refetch

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -4,7 +4,8 @@ import merge from "lodash/merge";
 
 import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
 import ResolutionsResolvers from "../../api/resolutions/resolvers";
-//hi
+
+//h
 
 const testSchema = `
 type Query {
